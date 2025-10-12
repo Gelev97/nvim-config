06d62049d333715f58d693cb354458d116cd4be3
Gelev97 <geruohai97@outlook.com>
2025-10-11 18:29:37 -0700

    better im-select for mac

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -1,11 +1,20 @@
 return {
-    "keaising/im-select.nvim",
-    lazy = false,
-    config = function()
-        require('im_select').setup({
-            default_command = "/opt/homebrew/bin/im-select",
-            set_default = true,  -- optional: set default input method
-        })
-    end,
-}
+  "keaising/im-select.nvim",
+  lazy = false,
+  config = function()
+    -- Start with a base/empty options table, which works for Ubuntu
+    local opts = {}
 
+    -- Check if the operating system is macOS
+    if vim.fn.has('mac') == 1 then
+      -- If it's a Mac, overwrite the options with your Mac-specific settings
+      opts = {
+        default_command = "/opt/homebrew/bin/im-select",
+        set_default = true,
+      }
+    end
+
+    -- Call the setup function with the correct options for the current OS
+    require('im_select').setup(opts)
+  end,
+}
