8979f011ff6b0dfeb2d745fdd7cddb12559224dc
Gelev97 <geruohai97@outlook.com>
2025-10-11 18:27:01 -0700

    test im-select mac

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -2,6 +2,10 @@ return {
     "keaising/im-select.nvim",
     lazy = false,
     config = function()
-        require('im_select').setup({})
+        require('im_select').setup({
+            default_command = "/opt/homebrew/bin/im-select",
+            set_default = true,  -- optional: set default input method
+        })
     end,
 }
+
