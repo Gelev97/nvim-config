9425ad26921c6f3089ab01754bd44ef55bd6fa49
Gelev97 <geruohai97@outlook.com>
2025-10-16 13:03:05 -0700

    add dap and fix startup

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -13,19 +13,24 @@ return {
       dap.adapters.cppdbg = { id = "cppdbg", type = "executable", command = "cpp-dap" }
       dap.adapters.python = { type = "executable", command = "python", args = { "-m", "debugpy.adapter" } }
 
-      -- ===================================================================
-      -- NEW: Clear existing configurations before loading from file
-      -- This is the definitive fix for duplicate entries.
       dap.configurations.python = {}
       dap.configurations.cpp = {}
-      -- ===================================================================
 
       require("dap.ext.vscode").load_launchjs(nil, {
         cpp = { "cppdbg" },
         python = { "python" },
       })
 
-      -- Your '<Leader>f' keymaps
+      local function find_and_close_dap_terminal()
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.api.nvim_buf_get_option(buf, 'buftype') == 'terminal' then
+            vim.schedule(function()
+              vim.cmd('bdelete! ' .. buf)
+            end)
+          end
+        end
+      end
+
       vim.g.mapleader = " "
       vim.keymap.set("n", "<Leader>fb", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
       vim.keymap.set("n", "<Leader>fc", function()
@@ -35,7 +40,39 @@ return {
       vim.keymap.set("n", "<Leader>fo", dap.step_over, { desc = "DAP: Step Over" })
       vim.keymap.set("n", "<Leader>fi", dap.step_into, { desc = "DAP: Step Into" })
       vim.keymap.set("n", "<Leader>fu", dap.step_out, { desc = "DAP: Step Out" })
-      vim.keymap.set("n", "<Leader>fq", dap.terminate, { desc = "DAP: Quit/Terminate" })
+
+      vim.keymap.set("n", "<Leader>fq", function()
+        dap.terminate()
+        dap.repl.close()
+        find_and_close_dap_terminal()
+      end, { desc = "DAP: Quit/Terminate & Close All Windows" })
+
+      vim.keymap.set("n", "<Leader>fe", dap.repl.open, { desc = "DAP: Open REPL (eval)" })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "python", "cpp", "c", "rust", "go", "java" },
+        callback = function()
+          vim.keymap.set("n", "K", function()
+            require("dap.ui.widgets").hover()
+          end, { buffer = true, desc = "DAP: Hover Variable" })
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "dap-repl",
+        callback = function(args)
+          vim.schedule(function()
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+              if vim.api.nvim_win_get_buf(win) == args.buf then
+                vim.api.nvim_set_current_win(win)
+                vim.cmd('startinsert')
+                break
+              end
+            end
+          end)
+        end,
+        desc = "Focus and start insert mode when DAP REPL opens",
+      })
     end,
   },
 }
